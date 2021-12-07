1e52c07286462c4137969af76ca5aafc0cdc4ae2
opilane <opilane@HP-CZC0359NN5>
2021-12-07 16:01:09 +0200

    first commit

---
+++ repos/titpe21/BitcoinCalculator/BitcoinCalculator/BitcoinRate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitcoinCalculator
+{
+    public class BitcoinRate
+    {
+        public bpi bpi { get; set;}
+    }
+
+
+    public class bpi
+    {
+        public EUR EUR { get; set; }
+    }
+
+    public class EUR
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    } 
+
+
+
+
+
+
+}
+
---
+++ repos/titpe21/BitcoinCalculator/BitcoinCalculator/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace BitcoinCalculator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BitcoinRate currentbitcoin = GetRate();
+            Console.WriteLine($"current rate: {currentbitcoin.bpi.EUR.code} {currentbitcoin.bpi.}
+
+            Console.WriteLine("Calculate in: EUR/USD/GBP");
+            string userChoice = Console.ReadLine();
+            Console.WriteLine("Enter the amount of bitcoins");
+            float userCoins = float.Parse(Console.WriteLine());
+            float currentRate = 0;
+
+            if(userChoice == "EUR")
+            {
+                currentRate = currentbitcoin.bpi.EUR.rate_float;
+            }
+
+            float result = currentRate * userCoins;
+            Console.WriteLine($"your bitcoins are worth {result {userChoise} ");
+
+
+
+
+        }
+
+        public static BitcoinRate GetRate()
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            BitcoinRate BitcoinData;
+
+            using(var responseReader = new StreamReader(webStream))
+            {
+                var reponse = responseReader.ReadToEnd();
+                BitcoinData = JsonConvert.DeserializeObject<BitcoinRate>(reponse);
+            }
+
+            return BitcoinData;
+
+        }
+
+    }
+}
